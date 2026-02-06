8ff4362224ba2fc726965206bb168a4b94cd6fbb
luiizz72 <169167567+luiizz72@users.noreply.github.com>
2026-02-05 22:49:13 -0300

    Add files via upload

---
+++ javascript/variaveis.js
@@ -0,0 +1,12 @@
+let nome = "João";
+var idade = 34;
+const idadeMinima =18;
+
+if (idade >= idadeMinima){
+    console.log('Vocè é maior idade');
+}else{
+    console.log('Você é menor de idade');
+}
+console.log("Nome:", nome);
+console.log("Idade:", idade);
+console.log("Idade Mínima:", idadeMinima);
